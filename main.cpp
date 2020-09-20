999bfa7fd752d10f7539e9af4159c2c61c694f5d
maxsupermanhd <q3.max.2011@ya.ru>
2020-09-20 14:11:02 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -123,13 +123,13 @@ void RenderPIE(struct PIEobject o, float rx, float ry, float rz) {
 	glRotatef(rz, 0.0f, 0.0f, 1.0f);
 	glBegin(GL_TRIANGLES);
 	//glCullFace(GL_FRONT);
-	glColor3f(255, 255, 255);
 	for(int i=0; i<o.polygonscount; i++) {
 		if(o.polygons[i].pcount != 3) {
 			log_error("Draw panic!");
 			abort();
 		}
 		for(int j=0; j<o.polygons[i].pcount; j++) {
+			glColor3f(rand()%255/(float)255, rand()%255/(float)255, rand()%255/(float)255);
 			glVertex3f(o.points[o.polygons[i].porder[j]].x/16, o.points[o.polygons[i].porder[j]].y/16, o.points[o.polygons[i].porder[j]].z/16);
 		}
 	}
@@ -192,79 +192,36 @@ int main(int argc, char** argv) {
 	// 					glm::vec3(0.0f, 0.0f, 0.0f),
 	// 					glm::vec3(0.0f, 1.0f, 0.0f));
 
-	float vertices[] = {
-	        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-	         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-	         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-	         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-	        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-	        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+    // unsigned int VBO, VAO;
+    // glGenVertexArrays(1, &VAO);
+    // glGenBuffers(1, &VBO);
+	//
+    // glBindVertexArray(VAO);
+	//
+    // glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	//
+    // // position attribute
+    // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    // glEnableVertexAttribArray(0);
+    // // texture coord attribute
+    // glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    // glEnableVertexAttribArray(1);
+	//
+	// unsigned int texture1;
+	// glGenTextures(1, &texture1);
+	// glBindTexture(GL_TEXTURE_2D, texture1);
+	// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// // log_info("%d %d %d %d", m.texture.width, m.texture.height, m.texture.rgbpixels[0], m.texture.rgbpixels[m.texture.width*m.texture.height*3-1]);
+	// glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, m.texture.width, m.texture.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, m.texture.rgbpixels);
+	// // log_info("glGenerateMipmap is %p", glGenerateMipmap);
+	// glGenerateMipmap(GL_TEXTURE_2D);
 
-	        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-	         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-	         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-	         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-	        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-	        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-
-	        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-	        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-	        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-	        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-	        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-	        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-	         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-	         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-	         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-	         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-	         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-	         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-	        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-	         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-	         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-	         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-	        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-	        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-
-	        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-	         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-	         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-	         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-	        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-	        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
-	    };
-    unsigned int VBO, VAO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    // texture coord attribute
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-
-	unsigned int texture1;
-	glGenTextures(1, &texture1);
-	glBindTexture(GL_TEXTURE_2D, texture1);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	// log_info("%d %d %d %d", m.texture.width, m.texture.height, m.texture.rgbpixels[0], m.texture.rgbpixels[m.texture.width*m.texture.height*3-1]);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, m.texture.width, m.texture.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, m.texture.rgbpixels);
-	// log_info("glGenerateMipmap is %p", glGenerateMipmap);
-	glGenerateMipmap(GL_TEXTURE_2D);
-
-	mshader shad("vertex.vs", "fragment.frag");
-	glUniform1i(glGetUniformLocation(shad.program, "texture1"), 1);
+	//mshader shad("vertex.vs", "fragment.frag");
+	//glUniform1i(glGetUniformLocation(shad.program, "texture1"), 1);
 	bool r=1;
 	SDL_Event ev;
 	float crx = 30.0f, cry = 0.0f, crz = 0.0f;
@@ -312,24 +269,24 @@ int main(int argc, char** argv) {
 
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glLoadIdentity();
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, texture1);
-		shad.use();
-		GLfloat radius = 10.0f;
-		GLfloat camX = sin(SDL_GetTicks()) * radius;
-		GLfloat camZ = cos(SDL_GetTicks()) * radius;
-		glm::mat4 view = glm::lookAt(glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 0.0, 0.0));
-		glm::mat4 projection = glm::perspective(45.0f, (GLfloat)width / (GLfloat)height, 0.1f, 400.0f);
-		GLint modelLoc = glGetUniformLocation(shad.program, "model");
-		GLint viewLoc = glGetUniformLocation(shad.program, "view");
-		GLint projLoc = glGetUniformLocation(shad.program, "projection");
-		glm::mat4 model = glm::translate(model, glm::vec3(0.0f, 0.0f, -35.0f));
-		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-		//RenderPIE(m, crx, cry, crz);
+        // glActiveTexture(GL_TEXTURE0);
+        // glBindTexture(GL_TEXTURE_2D, texture1);
+		// shad.use();
+		// GLfloat radius = 10.0f;
+		// GLfloat camX = sin(SDL_GetTicks()) * radius;
+		// GLfloat camZ = cos(SDL_GetTicks()) * radius;
+		// glm::mat4 view = glm::lookAt(glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 0.0, 0.0));
+		// glm::mat4 projection = glm::perspective(45.0f, (GLfloat)width / (GLfloat)height, 0.1f, 400.0f);
+		// GLint modelLoc = glGetUniformLocation(shad.program, "model");
+		// GLint viewLoc = glGetUniformLocation(shad.program, "view");
+		// GLint projLoc = glGetUniformLocation(shad.program, "projection");
+		// glm::mat4 model = glm::translate(model, glm::vec3(0.0f, 0.0f, -35.0f));
+		// glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		// glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+		// glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		//
+		// glDrawArrays(GL_TRIANGLES, 0, 36);
+		RenderPIE(m, crx, cry, crz);
 
 		glFlush();
 		SDL_GL_SwapWindow(window);
