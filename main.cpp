1ebba8f6c13d53ee815b701cc1855b32896b798b
bjorn-ali-goransson <bjorn.a.goransson@gmail.com>
2020-09-22 21:00:28 +0200

    Cleanup

--- main.cpp
+++ main.cpp
@@ -116,10 +116,9 @@ int main(int argc, char** argv) {
 	// glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	// glBufferData(GL_ARRAY_BUFFER, m.GLvertexesCount, m.GLvertexes, GL_STATIC_DRAW);
-	glVertexAttribPointer(glGetUniformLocation(shad.program, "t"), 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
+	glVertexAttribPointer(glGetAttribLocation(shad.program, "c"), 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(glGetAttribLocation(shad.program, "c"));
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
 	bool r=1;
 	SDL_Event ev;
