fe90971c5076b22e45055d5b3474373f4c6d276c
maxsupermanhd <q3.max.2011@ya.ru>
2020-09-22 15:57:14 +0300

    I broke everything

--- main.cpp
+++ main.cpp
@@ -123,6 +123,11 @@ int main(int argc, char** argv) {
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
+
+	glm::mat4 trans = matrix2 * matrix1;
+	unsigned int transformLoc = glGetUniformLocation(shad.program, "t");
+	glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
--- vertex.vs
+++ vertex.vs
@@ -1,7 +1,10 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
 
+uniform mat4 t;
+
 void main()
 {
-    gl_Position = vec4(aPos, 1.0f);
+    gl_Position = t * vec4(aPos, 1.0f);
+	// gl_Position = vec4(aPos, 1.0f);
 }
