3d50bc469d3193a648842d7920a99dfe5b017b8b
bjorn-ali-goransson <bjorn.a.goransson@gmail.com>
2020-09-22 22:32:33 +0200

    Cleanup

--- main.cpp
+++ main.cpp
@@ -102,15 +102,14 @@ int main(int argc, char** argv) {
 	glGenerateMipmap(GL_TEXTURE_2D);
 
 	mshader shad("vertex.vs", "fragment.frag");
+
 	unsigned int VBO_vertices, VAO_vertices;
 	glGenVertexArrays(1, &VAO_vertices);
 	glGenBuffers(1, &VBO_vertices);
 	glBindVertexArray(VAO_vertices);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO_vertices);
-
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	// glBufferData(GL_ARRAY_BUFFER, m.GLvertexesCount, m.GLvertexes, GL_STATIC_DRAW);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO_vertices);
 
 	glVertexAttribPointer(glGetAttribLocation(shad.program, "Coordinates"), 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(glGetAttribLocation(shad.program, "Coordinates"));
