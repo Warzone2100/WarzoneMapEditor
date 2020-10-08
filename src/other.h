3bf122a24955108857f82d41e7a284b04d611708
maxsupermanhd <q3.max.2011@ya.ru>
2020-10-08 19:04:51 +0300

    ;

--- src/other.h
+++ src/other.h
@@ -5,6 +5,6 @@
 
 size_t snprcat(char* str, size_t stroffs, size_t strmax, const char* format, ...);
 char* sprcatr(char* str, const char* format, ...);
-bool equalstr(char* s1, const char* s2)
+bool equalstr(char* s1, const char* s2);
 
 #endif /* end of include guard: OTHER_H_DEFINED */
